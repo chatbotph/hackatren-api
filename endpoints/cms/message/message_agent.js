b603317924bda09a1ac640622caeffc49b734cd9
Pons <lanzosuarez@gmail.com>
2018-05-29 14:39:59 +0800

    message agent revision

--- endpoints/cms/message/message_agent.js
+++ endpoints/cms/message/message_agent.js
@@ -1,24 +1,22 @@
 const Message = require("../../../models/message"),
   Thread = require("../../../models/thread"),
+  Order = require("../../../models/order"),
   {
-    errs: { CONFILCT_ERROR, SERVER_ERROR },
-    errMsgs: { SERVER_ERROR_MSG, CONFLICT_MSG }
+    errs: { NOT_FOUND, SERVER_ERROR },
+    errMsgs: { SERVER_ERROR_MSG, NOT_FOUND_MSG }
   } = require("../../../utils/errors"),
   { sendError, sendData } = require("../../../utils/uni-response"),
   { decodeToken } = require("../../../utils/security"),
   { isNotExists } = require("../../../utils/op-helpers"),
- 
   {
     Types: { ObjectId }
   } = require("mongoose");
 
 module.exports = (req, res, next) => {
-  const { agent } = req.query;
+  const { order_no } = req.query;
 
-  const createMessage = () => {
-    const newMsg = new Message(
-      Object.assign({ timestamp: Date.now() }, req.body)
-    );
+  const createMessage = message => {
+    const newMsg = new Message(message);
     return newMsg.save().catch(err => {
       throw err;
     });
@@ -26,24 +24,41 @@ module.exports = (req, res, next) => {
 
   async function main() {
     try {
-      let { type, message, status, timestamp, thread } = await createMessage();
-      let { order } = await Thread.findById(thread, "order").populate({
-        path: "order",
-        select: "order_no"
-      });
+      let order = await Order.findOne({ order_no });
+      if (isNotExists(order)) {
+        sendError(res, NOT_FOUND, NOT_FOUND_MSG);
+      } else {
+        let checkThread = await Thread.findOne({
+          order: ObjectId(order._id),
+          status: 1
+        });
+        if (isNotExists(checkThread)) {
+          sendError(res, NOT_FOUND, NOT_FOUND_MSG);
+        } else {
+          const { _id: threadId } = checkThread;
+          const msg = Object.assign({ thread: checkThread._id }, req.body);
+          let {
+            type,
+            message,
+            status,
+            timestamp,
+            thread
+          } = await createMessage(msg);
 
-      req.payload = {
-        message: {
-          type,
-          message,
-          status,
-          timestamp,
-          thread
-        },
-        order_no: order.order_no,
-        agent
-      };
-      next();
+          req.payload = {
+            message: {
+              type,
+              message,
+              status,
+              timestamp,
+              thread
+            },
+            order_no: order.order_no,
+            agent: "5af3db1271f54735bccdacbb"
+          };
+          next();
+        }
+      }
     } catch (error) {
       console.error(error);
       sendError(res, SERVER_ERROR, SERVER_ERROR_MSG);
--- utils/notifs.js
+++ utils/notifs.js
@@ -1,10 +1,10 @@
 exports.messages = {
   PENDING: order_no =>
-    `Your order with ref no: ${order_no}, is being assessed.`,
+    `Your order with order no: ${order_no}, is being assessed.`,
   FOR_DELIVERY: order_no =>
-    `Your order with ref no: ${order_no}, is being delivered.`,
+    `Your order with order no: ${order_no}, is being delivered.`,
   DELIVERED: order_no =>
-    `Your order with ref no: ${order_no}, is already delivered. Thank you for patronizing Jollibeee.`,
+    `Your order with order no: ${order_no}, is already delivered. Thank you for patronizing Jollibeee.`,
   REJECTED: order_no =>
-    `Your order with ref no: ${order_no}, has been cancelled.`
+    `Your order with order no: ${order_no}, has been cancelled.`
 };
